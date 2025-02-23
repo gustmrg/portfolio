27da809517e1bd4178f980fd9d8a76b890819b3e
Gustavo Miranda <gustmrg@gmail.com>
2025-02-23 16:24:54 -0300

    fix: display name in mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,10 @@ export default function Page() {
     <div className="min-h-screen bg-background">
       <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container mx-auto px-4 md:px-6 max-w-7xl flex h-14 items-center">
+          <Link className="mr-6 flex items-center space-x-2" href="/">
+            <span className="font-bold sm:inline-block">Gustavo Miranda</span>
+          </Link>
           <div className="mr-4 hidden md:flex">
-            <Link className="mr-6 flex items-center space-x-2" href="/">
-              <span className="hidden font-bold sm:inline-block">
-                Gustavo Miranda
-              </span>
-            </Link>
             <nav className="flex items-center space-x-6 text-sm font-medium">
               <Link
                 href="#about"
